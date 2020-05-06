6626fad0a90b74067ad9caf8e948bed16cc53480
i-whammy <yas.iwami@gmail.com>
2020-05-06 15:42:14 +0900

    integrated updateArticle to ktor

--- src/main/kotlin/com/whammy/article/handler/ArticleHandler.kt
+++ src/main/kotlin/com/whammy/article/handler/ArticleHandler.kt
@@ -3,6 +3,7 @@ package com.whammy.article.handler
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonRootName
 import com.whammy.article.domain.Article
+import com.whammy.article.domain.Comment
 import com.whammy.article.usecase.ArticleUsecase
 import com.whammy.user.service.UserService
 import io.ktor.application.call
@@ -11,12 +12,14 @@ import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.post
+import io.ktor.routing.put
 import java.time.LocalDateTime
 
 fun Route.articleHandler(articleUsecase: ArticleUsecase, userService: UserService) {
     get("/api/articles") {
         call.respond(
-            articleUsecase.getArticlesOrderedByUpdatedDateTime().map { it.convertToArticleResponse() }.let{ ArticlesResponse(it, it.size) })
+            articleUsecase.getArticlesOrderedByUpdatedDateTime().map { it.convertToArticleResponse() }
+                .let { ArticlesResponse(it, it.size) })
     }
 
     post("/api/articles") {
@@ -24,15 +27,20 @@ fun Route.articleHandler(articleUsecase: ArticleUsecase, userService: UserServic
         val authorizationHeader = call.request.headers["Authorization"]!!
         val userId = userService.getUserId(authorizationHeader)
         call.respond(
-            articleUsecase.createNewArticle(userId, request.title, request.body).convertToArticleResponse())
+            articleUsecase.createNewArticle(userId, request.title, request.body).convertToArticleResponse()
+        )
     }
-//
-//    @RequestMapping("/", method = [RequestMethod.POST])
-//    fun createArticle(@RequestHeader("Authorization", required = true) authorizationHeader: String, @RequestBody article: ArticleRequest): ResponseEntity<ArticleResponse> {
-//        val userId = userService.getUserId(authorizationHeader)
-//        return ResponseEntity.ok(articleUsecase.createNewArticle(userId, article.title, article.body).convertToArticleResponse())
-//    }
-//
+
+    put("/api/articles/{slug}") {
+        val request = call.receive<ArticleRequest>()
+        val slug = call.parameters["slug"]!!
+        val authorizationHeader = call.request.headers["Authorization"]!!
+        val userId = userService.getUserId(authorizationHeader)
+        call.respond(
+            articleUsecase.updateArticle(slug, userId, request.title, request.body).convertToArticleResponse()
+        )
+    }
+
 //    @RequestMapping("/{slug}", method = [RequestMethod.PUT])
 //    fun updateArticle(@RequestHeader("Authorization", required = true) authorizationHeader: String, @PathVariable slug: String, @RequestBody article: UpdateArticleRequest): ResponseEntity<ArticleResponse> {
 //        val userId = userService.getUserId(authorizationHeader)
@@ -72,32 +80,28 @@ fun Route.articleHandler(articleUsecase: ArticleUsecase, userService: UserServic
 //    fun deleteArticle(@PathVariable("slug") slug: String) {
 //        articleUsecase.delete(slug)
 //    }
-//
-//    private fun Article.convertToArticleResponse() : ArticleResponse {
-//        return ArticleResponse(slug, title, body, createdAt, favorites.isNotEmpty(), favorites.count())
-//    }
-//
-//    private fun Comment.convertToCommentResponse() : CommentResponse {
-//        return CommentResponse(id, body, createdAt, updatedAt)
-//    }
-    }
+}
 
 
-private fun Article.convertToArticleResponse() : ArticleResponse {
+private fun Article.convertToArticleResponse(): ArticleResponse {
     return ArticleResponse(slug, title, body, createdAt, favorites.isNotEmpty(), favorites.count())
 }
 
-    @JsonRootName("article")
-    data class ArticleRequest(
-        @JsonProperty("title", required = true) val title: String,
-        @JsonProperty("body", required = true) val body: String
-    )
+private fun Comment.convertToCommentResponse(): CommentResponse {
+    return CommentResponse(id, body, createdAt, updatedAt)
+}
 
-    @JsonRootName("article")
-    data class UpdateArticleRequest(
-        @JsonProperty("title") val title: String,
-        @JsonProperty("body") val body: String
-    )
+@JsonRootName("article")
+data class ArticleRequest(
+    @JsonProperty("title", required = true) val title: String,
+    @JsonProperty("body", required = true) val body: String
+)
+
+@JsonRootName("article")
+data class UpdateArticleRequest(
+    @JsonProperty("title") val title: String,
+    @JsonProperty("body") val body: String
+)
 
 data class ArticlesResponse(
     val articles: List<ArticleResponse>,
@@ -113,19 +117,19 @@ data class ArticleResponse(
     val favoritesCount: Int
 )
 
-    @JsonRootName("comment")
-    data class CommentRequest(
-        @JsonProperty("body", required = true) val body: String
-    )
+@JsonRootName("comment")
+data class CommentRequest(
+    @JsonProperty("body", required = true) val body: String
+)
 
-    data class CommentsResponse(
-        @JsonProperty("comments") val comments: List<CommentResponse>
-    )
+data class CommentsResponse(
+    @JsonProperty("comments") val comments: List<CommentResponse>
+)
 
-    @JsonRootName("comment")
-    data class CommentResponse(
-        @JsonProperty("id") val id: Int,
-        @JsonProperty("body") val body: String,
-        @JsonProperty("createdAt") val createdAt: LocalDateTime,
-        @JsonProperty("updatedAt") val updatedAt: LocalDateTime?
-    )
+@JsonRootName("comment")
+data class CommentResponse(
+    @JsonProperty("id") val id: Int,
+    @JsonProperty("body") val body: String,
+    @JsonProperty("createdAt") val createdAt: LocalDateTime,
+    @JsonProperty("updatedAt") val updatedAt: LocalDateTime?
+)
